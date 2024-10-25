65732f8dbdd8fab26ffb0aa025109799ade3a21e
Bhupali Nagane <bhupalivn@gmail.com>
2024-10-25 15:16:09 +0200

    cleaning up the mess

---
+++ Virtual Machine/outputs.tf
@@ -0,0 +1,24 @@
+output "resource_group_id" {
+  description = "The ID of the existing resource group"
+  value       = data.azurerm_resource_group.existingResourceGroup.id
+}
+
+output "virtual_network_id" {
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.azVNet.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = azurerm_subnet.azSubnet.id
+}
+
+output "network_interface_id" {
+  description = "The ID of the network interface"
+  value       = azurerm_network_interface.azNIC.id
+}
+
+output "virtual_machine_id" {
+  description = "The ID of the virtual machine"
+  value       = azurerm_linux_virtual_machine.azVM.name
+}
